50be9a820a3c6c517adcf757e89d900777763b52
Beato Bongco <beatobongco@gmail.com>
2015-01-04 17:13:45 +0800

    Create test for CRUD

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
 from web.app import db
-from web.controllers import create_post
+from web.controllers import create_post, update_post, delete_post
+from web.models import Post
 
-db.create_all()
+# Delete all posts, clean slate
+
+Post.query.delete()
+db.session.commit()
+
 create_post('Test Post', 'Test body', 'Test Cat')
+create_post('Test Post 2', 'Test body 2', 'Test Cat 2')
+update_post(1, 'Updated Post', 'Updated Body', 'Updated Catty')
+delete_post(2)
 
-# Expected results:
+# Expected results: '1 | Today's date i.e. 2015-01-04 09:12:53.928927 | Updated Post | Updated Body | Updated Catty'
--- web/views.py
+++ web/views.py
@@ -3,6 +3,7 @@ from controllers import get_all_posts
 
 @app.route("/")
 def hello():
+  """ Temporary debugging function """
   result = ""
   for i in get_all_posts():
     result = result + "{0} | {1} | {2} | {3} | {4} <br>".format(i.id, i.pub_date, i.title, i.body, i.category)
