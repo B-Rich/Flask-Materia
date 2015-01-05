71669dda28d3a77b43376c3c0a1ca86baf3541da
Beato Bongco <beatobongco@gmail.com>
2015-01-06 02:31:40 +0800

    Add form fields

--- web/static/js/editor.js
+++ web/static/js/editor.js
@@ -5,31 +5,55 @@ var converter = new Showdown.converter();
 var MarkdownEditor = React.createClass({
   getInitialState: function() {
     return {
-      value: 'Type some *markdown* here!'
+      value: 'Type some *markdown* here!',
+      cbValue: '0'
     };
   },
   handleChange: function() {
     this.setState({value: this.refs.textarea.getDOMNode().value});
   },
+  checkboxChange: function() {
+    this.setState({cbValue: event.target.value});
+  },
   componentDidMount: function() {
-    console.log("!");
     $('.editor-textarea').autosize();
   },
   render: function() {
     return (
       <div className="MarkdownEditor">
         <div className="row">
+          <div className="input-field col s12">
+            <input id="post_title" type="text" class="validate" />
+            <label htmlFor="post_title">Post Title</label>
+          </div>
+          <div className="input-field col s12">
+            <input id="flavor_text" type="text" class="validate" />
+            <label htmlFor="flavor_text">Flavor Text (160 characters)</label>
+          </div>
+          <div className="input-field col s6">
+            <input id="img_url" type="text" class="validate" />
+            <label htmlFor="img_url">Image URL</label>
+          </div>
+          <div className="input-field col s6">
+            <input id="categories" type="text" class="validate" />
+            <label htmlFor="categories">Categories</label>
+          </div>
+        </div>
+        <div className="row">
           <div className="editor-input col s12 m6 l6">
-            <h3>Input</h3>
+            <h5>Input</h5>
             <textarea
               className="editor-textarea"
               onChange={this.handleChange}
               ref="textarea"
               defaultValue={this.state.value} />
+            <button className="btn waves-effect waves-light btn-submit">Submit
+              <i className="mdi-content-send right"></i>
+            </button>
           </div>
 
           <div className="editor-output col m6 l6 hide-on-small-only">
-            <h3>Output</h3>
+            <h5>Output</h5>
             <div
               className="content"
               dangerouslySetInnerHTML={{
@@ -38,9 +62,6 @@ var MarkdownEditor = React.createClass({
             />
           </div>
         </div>
-        <button className="btn waves-effect waves-light">Submit
-          <i className="mdi-content-send right"></i>
-        </button>
       </div>
     );
   }
